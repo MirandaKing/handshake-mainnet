894e70a1034ace598aac0bf604ab02d983832a86
MirandaKing <miranda20199712@gmail.com>
2024-11-28 16:40:53 +0530

    resolve import error

--- app/components/SingleTranscationAccordianExpanded.jsx
+++ app/components/SingleTranscationAccordianExpanded.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./singletxexpanded.css";
 import { formatUnits } from "viem";
 import AddressWithCopy from "../quickaccess/AddressWithCopy";
